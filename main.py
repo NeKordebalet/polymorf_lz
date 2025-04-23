e4afe3ea7ad3913552ec4137c21bae16d23db694
NeKordebalet <nekordebalet2@tutamail.com>
2025-04-23 22:47:05 +0300

    6789

---
+++ main.py
@@ -0,0 +1,8 @@
+import polymorf as ca
+
+
+def main():
+    ca.main()
+
+if __name__ == '__main__':
+    main()
---
+++ polymorf.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+class Cash:
+
+    def data(self):
+        df = pd.read_csv('var10.csv')
+        df1 = df[df['Сумма cash-back'] == 0.00]    
+        df2 = df[df['Сумма cash-back'] > 0.00]    
+        df1.to_csv('f1.csv') 
+        df2.to_csv('f2.csv')
+        self.duplicates1 = df1.duplicated()
+        self.duplicates2 = df2.duplicated()
+    def __invert__(self):
+        self.num_duplicates1 = self.duplicates1.sum()
+        self.num_duplicates2 = self.duplicates2.sum()
+        num = self.num_duplicates1 + self.num_duplicates2
+        print(num)
+    def __del__(self):
+        print("DEL")
+def main(): 
+    pro = Cash()
+    pro.data()
+    ~pro
+if __name__ == "__main__":
+    main()
